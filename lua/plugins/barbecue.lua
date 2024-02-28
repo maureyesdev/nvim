b6e1ba21c1442aff33c831f0b599102ea7285a96
maureyesdev <maureyes.dev@gmail.com>
2024-02-28 00:15:45 -0600

    feat: adds nvim-tree

---
+++ lua/plugins/barbecue.lua
@@ -0,0 +1,13 @@
+-- TODO: check this plugin out -> Is a breadcrumbs location of the file
+return {
+	--"utilyre/barbecue.nvim",
+	--name = "barbecue",
+	--version = "*",
+	--dependencies = {
+	--"SmiteshP/nvim-navic",
+	--"nvim-tree/nvim-web-devicons", -- optional dependency
+	--},
+	--opts = {
+	-- configurations go here
+	--},
+}
