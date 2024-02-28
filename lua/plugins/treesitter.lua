13dc69f24b41cc4448baf43b5b8260fd70eb2d96
maureyesdev <maureyes.dev@gmail.com>
2024-02-27 23:15:04 -0600

    feat: adds treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,21 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  dependencies = {
-    -- 'nvim-treesitter/nvim-treesitter-textobjects',
-  },
-  config = function()
-    treesitter_config = require("nvim-treesitter.configs")
-    treesitter_config.setup({})
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		-- 'nvim-treesitter/nvim-treesitter-textobjects',
+	},
+	config = function()
+		local treesitter_config = require("nvim-treesitter.configs")
+		treesitter_config.setup({
+			modules = {},
+			ensure_installed = { "javascript", "typescript", "python", "c", "lua", "vim", "vimdoc", "query" },
+			sync_install = false,
+			ignore_install = {},
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+				disable = {},
+			},
+		})
+	end,
 }
