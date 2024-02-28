c6c5a061128c2ff1b34d973a96fb24eb889a7c80
maureyesdev <maureyes.dev@gmail.com>
2024-02-27 20:08:33 -0600

    feat: adds lsp and cmp

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,60 @@
+return {
+  -- Autocompletion
+  { 'hrsh7th/cmp-buffer' },
+  { 'hrsh7th/cmp-path' },
+  { 'hrsh7th/cmp-cmdline' },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "saadparwaiz1/cmp_luasnip"
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      luasnip.config.setup({})
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert {
+          ['<C-j>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-k>'] = cmp.mapping.scroll_docs(4),
+          ['<C-b>'] = cmp.mapping.complete {},
+          ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          },
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        },
+
+        sources = {
+          { name = "luasnip" },
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "nvim_lua" },
+          { name = "path" },
+        },
+      })
+    end
+  },
+}
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,86 @@
+return {
+  -- Setup neovim lua configuration
+  {
+    "folke/neodev.nvim",
+    config = function()
+      require("neodev").setup({})
+    end
+  },
+  {
+    -- LSP Configuration & Plugins
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      { "williamboman/mason.nvim" } ,
+      { "williamboman/mason-lspconfig.nvim" },
+      { "j-hui/fidget.nvim", opts = {} },
+      { "folke/neodev.nvim" }
+    },
+    config = function()
+      local servers = {
+        clangd = {},
+        pyright = {
+          python = {
+            analysis = {
+              typeCheckingMode = "off",
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true
+            }
+          }
+        },
+        -- FOR VUE
+        -- volar = {
+        --   filetypes = {'vue', 'typescript', 'javascript', 'json'},
+        -- },
+        rust_analyzer = {},
+        tsserver = {
+          filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "javascript.jsx" },
+          cmd = { "typescript-language-server", "--stdio" }
+        },
+        -- tailwindcss = {
+        --   filetypes = {"html", "css", "scss"}
+        -- },
+        cssls = {
+          filetypes = {"vue", "css", "scss"}
+        },
+
+        lua_ls = {
+          Lua = {
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
+          },
+        },
+      }
+      -- TODO: NEED TO FIGURE HOW TO RESOLVE `vim` VARIABLE TO BE VALID
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+      local on_attach = function(client, _) -- (client, bufnr)
+        if (client.name == "tsserver") or (client.name == "tailwindcss") then
+          client.server_capabilities.documentFormattingProvider = false
+          client.server_capabilities.documentRangeFormattingProvider = false
+        else
+          client.server_capabilities.documentFormattingProvider = true
+          client.server_capabilities.documentRangeFormattingProvider = true
+        end
+        -- Create a command `:Format` local to the LSP buffer
+      end
+
+      -- Setup mason so it can manage external tooling
+      require('mason').setup()
+      -- Ensure the servers above are installed
+      local mason_lspconfig = require('mason-lspconfig')
+      mason_lspconfig.setup({
+        ensure_installed = vim.tbl_keys(servers),
+      })
+
+      mason_lspconfig.setup_handlers({
+        function(server_name)
+          require('lspconfig')[server_name].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = servers[server_name],
+          })
+        end,
+      })
+    end
+  },
+}
