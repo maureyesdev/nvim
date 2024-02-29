29abd1b2052767e76a232fbd9fe36ede20fb4fa2
maureyesdev <maureyes.dev@gmail.com>
2024-02-28 21:59:05 -0600

    feat: adds better comments

---
+++ lua/plugins/better-comments.lua
@@ -0,0 +1,8 @@
+return {
+	"Djancyp/better-comments.nvim",
+	config = function()
+		local better_comments = require("better-comment")
+		-- TODO: NEED TO SET THIS UP
+		better_comments.Setup({})
+	end,
+}
