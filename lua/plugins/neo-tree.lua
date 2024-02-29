b15220811ee1487b5726e04a99c696adaf365841
maureyesdev <maureyes.dev@gmail.com>
2024-02-28 21:56:18 -0600

    feat: adds lualine and amend width of neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,32 +1,32 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    local neo_tree = require("neo-tree")
-    neo_tree.setup({
-      window = {
-        position = "right",
-        width = 80,
-      },
-      filesystem = {
-        visible = false,
-        show_hidden_count = true,
-        hide_dotfiles = false,
-        hide_gitignore = false,
-      }
-    })
-  end,
-  keys = {
-    {
-      "<Leader>e",
-      function()
-        require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
-      end
-    }
-  }
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		local neo_tree = require("neo-tree")
+		neo_tree.setup({
+			window = {
+				position = "right",
+				width = 40,
+			},
+			filesystem = {
+				visible = false,
+				show_hidden_count = true,
+				hide_dotfiles = false,
+				hide_gitignore = false,
+			},
+		})
+	end,
+	keys = {
+		{
+			"<Leader>e",
+			function()
+				require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+			end,
+		},
+	},
 }
