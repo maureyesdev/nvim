b98dd4d37c3631150de8a3914e6c65e86aed164e
maureyesdev <maureyes.dev@gmail.com>
2024-03-01 22:50:18 -0600

    feat: adds keymaps for treesitter incremental and decremental selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,21 +1,30 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		-- 'nvim-treesitter/nvim-treesitter-textobjects',
-	},
-	config = function()
-		local treesitter_config = require("nvim-treesitter.configs")
-		treesitter_config.setup({
-			modules = {},
-			ensure_installed = { "javascript", "typescript", "python", "c", "lua", "vim", "vimdoc", "query" },
-			sync_install = false,
-			ignore_install = {},
-			auto_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-				disable = {},
-			},
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    -- 'nvim-treesitter/nvim-treesitter-textobjects',
+  },
+  config = function()
+    local treesitter_config = require("nvim-treesitter.configs")
+    treesitter_config.setup({
+      modules = {},
+      ensure_installed = { "javascript", "typescript", "python", "c", "lua", "vim", "vimdoc", "query" },
+      sync_install = false,
+      ignore_install = {},
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+        disable = {},
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    })
+  end,
 }
