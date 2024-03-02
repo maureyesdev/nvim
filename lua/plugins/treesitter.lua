7a1107ad97f13b41a66584c6f7cbe6b605afdcae
maureyesdev <maureyes.dev@gmail.com>
2024-03-01 23:05:22 -0600

    feat: adds auto closing and auto rename html tags on treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,30 +1,40 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    -- 'nvim-treesitter/nvim-treesitter-textobjects',
+  --  Automatically add closing tags for HTML and JSX
+  {
+    "windwp/nvim-ts-autotag",
   },
-  config = function()
-    local treesitter_config = require("nvim-treesitter.configs")
-    treesitter_config.setup({
-      modules = {},
-      ensure_installed = { "javascript", "typescript", "python", "c", "lua", "vim", "vimdoc", "query" },
-      sync_install = false,
-      ignore_install = {},
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-        disable = {},
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      -- 'nvim-treesitter/nvim-treesitter-textobjects',
+      "windwp/nvim-ts-autotag",
+    },
+    config = function()
+      local treesitter_config = require("nvim-treesitter.configs")
+      treesitter_config.setup({
+        modules = {},
+        ensure_installed = { "javascript", "typescript", "python", "c", "lua", "vim", "vimdoc", "query" },
+        sync_install = false,
+        ignore_install = {},
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+          disable = {},
         },
-      },
-    })
-  end,
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+        autotag = {
+          enable = true,
+        }
+      })
+    end,
+  }
 }
