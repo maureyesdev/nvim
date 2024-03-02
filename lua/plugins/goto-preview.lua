dc4b86e3790a996af14af5ff1710d9f7c4639506
maureyesdev <maureyes.dev@gmail.com>
2024-03-01 22:47:35 -0600

    feat: adds keymaps for goto-preview

--- lua/plugins/goto-preview.lua
+++ lua/plugins/goto-preview.lua
@@ -1,8 +1,16 @@
-return {}
--- return {
--- 	"rmagatti/goto-preview",
--- 	config = function()
--- 		local goto_preview = require("goto-preview")
--- 		goto_preview.setup({})
--- 	end,
--- }
+return {
+  "rmagatti/goto-preview",
+  config = function()
+    local goto_preview = require("goto-preview")
+    goto_preview.setup({})
+    -- When open the preview, `one can manipulate floating windows with the regular vim
+    -- window moving commands`. See `:h window-moving`
+    -- C-w + H || C-w + L
+    vim.keymap.set(
+      "n",
+      "<Leader>gpd",
+      "<cmd>lua require('goto-preview').goto_preview_definition()<CR>",
+      { noremap = true }
+    )
+  end,
+}
