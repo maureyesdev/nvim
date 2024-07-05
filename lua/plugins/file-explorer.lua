842b034cfc7732d83e83258a75556b17ffe28b08
maureyesdev <maureyes.dev@gmail.com>
2024-07-05 01:41:15 -0600

    feat: added file explorer

---
+++ lua/plugins/file-explorer.lua
@@ -0,0 +1,38 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  keys = {
+    -- TODO: need to move this to keymap file
+    {
+      "<Leader>e",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+      end,
+    },
+  },
+  config = function()
+    local neo_tree = require("neo-tree")
+    local neo_tree_command = require("neo-tree.command")
+    neo_tree.setup({
+      window = {
+        -- position = "right",
+        position = "float",
+      },
+      event_handlers = {
+        -- TODO: Need to find a better place to put his
+        -- This handler is in order to close neo-tree after open a file
+        {
+          event = "file_opened",
+          handler = function()
+            neo_tree_command.execute({ action = "close" })
+          end
+        }
+      }
+    })
+  end,
+}
