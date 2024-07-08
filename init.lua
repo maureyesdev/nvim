18a38ce8f408aa4acf90bb94f38cfbc6b54e226b
maureyesdev <maureyes.dev@gmail.com>
2024-07-08 11:08:09 -0600

    feat: add some vim utilities for yank and background none

--- init.lua
+++ init.lua
@@ -1,1 +1,16 @@
 require("config")
+
+-- TODO: Need to find out a better place for this
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+-- TODO: NEED TO FIND A BETTER FILE TO ADD THIS IN
+-- Utility in order to highlight when copy
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", {}),
+  desc = "Hightlight selection on yank",
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 50 })
+  end,
+})
