da2cd314300d6f5c6f51fc96707ea6d97b4bbcb8
maureyesdev <maureyes.dev@gmail.com>
2024-07-24 11:05:29 -0600

    feat: add and activate night-owl theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,7 +1,18 @@
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  config = function()
-    vim.cmd("colorscheme rose-pine")
-  end,
+  -- {
+  --   "rose-pine/neovim",
+  --   name = "rose-pine",
+  --   config = function()
+  --     vim.cmd("colorscheme rose-pine")
+  --   end,
+  -- },
+  {
+    "oxfist/night-owl.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("night-owl").setup()
+      vim.cmd.colorscheme("night-owl")
+    end,
+  },
 }
