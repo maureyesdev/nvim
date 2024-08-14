a0a7b78560ef2a36a8141f6509f0b10d77e7a653
maureyesdev <maureyes.dev@gmail.com>
2024-08-14 12:28:25 -0600

    feat: added catpuccin as theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,16 +3,34 @@ return {
   --   "rose-pine/neovim",
   --   name = "rose-pine",
   --   config = function()
-  --     vim.cmd("colorscheme rose-pine")
+  --     vim.cmd.colorscheme("rose-pine")
+  --   end,
+  -- },
+  -- {
+  --   "oxfist/night-owl.nvim",
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     require("night-owl").setup()
+  --     vim.cmd.colorscheme("night-owl")
   --   end,
   -- },
   {
-    "oxfist/night-owl.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    "catppuccin/nvim",
+    name = "catppuccin",
     config = function()
-      require("night-owl").setup()
-      vim.cmd.colorscheme("night-owl")
+      require("catppuccin").setup({
+        flavour = "mocha",
+        color_overrides = {
+          mocha = {
+            base = "#000000",
+            mantle = "#000000",
+            crust = "#000000",
+          },
+        },
+      })
+
+      vim.cmd.colorscheme("catppuccin")
     end,
   },
 }
