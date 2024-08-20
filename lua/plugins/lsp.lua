88091321af101bed055b96104242ca08d9802bfb
maureyesdev <maureyes.dev@gmail.com>
2024-08-19 19:28:24 -0600

    feat: added renamer for rename vars

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -56,4 +56,14 @@ return {
       })
     end,
   },
+  {
+    "filipdutescu/renamer.nvim",
+    branch = "master",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("renamer").setup({})
+    end,
+  },
 }
