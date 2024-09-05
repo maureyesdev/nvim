155f4646ffd11ee6abbceb84f1b6fc57ce760803
maureyesdev <maureyes.dev@gmail.com>
2024-09-04 23:01:26 -0600

    feat: alter default theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,20 +1,4 @@
 return {
-  -- {
-  --   "rose-pine/neovim",
-  --   name = "rose-pine",
-  --   config = function()
-  --     vim.cmd.colorscheme("rose-pine")
-  --   end,
-  -- },
-  -- {
-  --   "oxfist/night-owl.nvim",
-  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  --   priority = 1000, -- make sure to load this before all the other start plugins
-  --   config = function()
-  --     require("night-owl").setup()
-  --     vim.cmd.colorscheme("night-owl")
-  --   end,
-  -- },
   {
     "catppuccin/nvim",
     name = "catppuccin",
@@ -24,8 +8,7 @@ return {
         color_overrides = {
           mocha = {
             base = "#000000",
-            mantle = "#000000",
-            crust = "#000000",
+            mantle = "#181825",
           },
         },
       })
