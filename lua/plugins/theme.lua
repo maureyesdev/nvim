2762ac2d07575b4fa0bac8926f028dd36ff52ff3
maureyesdev <maureyes.dev@gmail.com>
2024-09-06 23:15:36 -0600

    feat: add a little custom rose-pine theme as default

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,19 +1,50 @@
 return {
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   config = function()
+  --     require("catppuccin").setup({
+  --       flavour = "mocha",
+  --       color_overrides = {
+  --         mocha = {
+  --           base = "#000000",
+  --           mantle = "#000000",
+  --         },
+  --       },
+  --     })
+  --
+  --     vim.cmd.colorscheme("catppuccin")
+  --   end,
+  -- },
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "rose-pine/neovim",
+    name = "rose-pine",
     config = function()
-      require("catppuccin").setup({
-        flavour = "mocha",
-        color_overrides = {
-          mocha = {
+      require("rose-pine").setup({
+        variant = "main",
+        dark_variant = "main",
+        palette = {
+          main = {
             base = "#000000",
-            mantle = "#181825",
+            surface = "#000000",
+            -- overlay = "#191724",
+            overlay = "#000000",
           },
         },
       })
 
-      vim.cmd.colorscheme("catppuccin")
+      vim.cmd.colorscheme("rose-pine")
+
+      local set_hl = vim.api.nvim_set_hl
+      set_hl(0, "IblIndent", { fg = "#191724" })
+      set_hl(0, "ColorColumn", { bg = "#191724" })
+      set_hl(0, "Folded", { bg = "#191724" })
+      set_hl(0, "NormalFloat", { bg = "#191724" })
+      set_hl(0, "TelescopeNormal", { bg = "#000000" })
+      set_hl(0, "TelescopeTitle", { bg = "#191724", fg = "#ebbcba" })
+      set_hl(0, "TelescopeSelection", { bg = "#191724" })
+      set_hl(0, "MiniIndentscopeSymbol", { bg = "None", fg = "#ebbcba" })
+      set_hl(0, "CursorLine", { bg = "#191724" })
     end,
   },
 }
