959aea4c54f36d65de7bcb33907a3de849f5d0cb
maureyesdev <maureyes.dev@gmail.com>
2024-09-09 15:48:54 -0600

    feat: added values of theme instead of nvim api

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -25,26 +25,40 @@ return {
         dark_variant = "main",
         palette = {
           main = {
+            _nc = "#16141f",
             base = "#000000",
             surface = "#000000",
-            -- overlay = "#191724",
             overlay = "#000000",
+            muted = "#6e6a86",
+            subtle = "#908caa",
+            text = "#e0def4",
+            love = "#eb6f92",
+            gold = "#f6c177",
+            rose = "#ebbcba",
+            pine = "#31748f",
+            foam = "#9ccfd8",
+            iris = "#c4a7e7",
+            leaf = "#95b1ac",
+            highlight_low = "#21202e",
+            highlight_med = "#403d52",
+            highlight_high = "#524f67",
+            none = "NONE",
           },
+        },
+        highlight_groups = {
+          IblIndent = { fg = "#26233a" },
+          ColorColumn = { bg = "#1f1d2e" },
+          Folded = { bg = "#1f1d2e" },
+          NormalFloat = { bg = "#191724" },
+          TelescopeNormal = { bg = "#000000" },
+          TelescopeTitle = { bg = "#191724", fg = "#ebbcba" },
+          TelescopeSelection = { bg = "#191724" },
+          MiniIndentscopeSymbol = { bg = "None", fg = "#ebbcba" },
+          CursorLine = { bg = "#191724" },
         },
       })
 
       vim.cmd.colorscheme("rose-pine")
-
-      local set_hl = vim.api.nvim_set_hl
-      set_hl(0, "IblIndent", { fg = "#191724" })
-      set_hl(0, "ColorColumn", { bg = "#191724" })
-      set_hl(0, "Folded", { bg = "#191724" })
-      set_hl(0, "NormalFloat", { bg = "#191724" })
-      set_hl(0, "TelescopeNormal", { bg = "#000000" })
-      set_hl(0, "TelescopeTitle", { bg = "#191724", fg = "#ebbcba" })
-      set_hl(0, "TelescopeSelection", { bg = "#191724" })
-      set_hl(0, "MiniIndentscopeSymbol", { bg = "None", fg = "#ebbcba" })
-      set_hl(0, "CursorLine", { bg = "#191724" })
     end,
   },
 }
