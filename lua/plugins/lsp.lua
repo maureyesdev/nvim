0ac1969bc3714e6dac761e992bf268f4898a99cd
maureyesdev <maureyes.dev@gmail.com>
2024-12-19 00:20:28 -0600

    feat(lsp): add better diagnostic

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,39 @@
 return {
   {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    config = function()
+      require("tiny-inline-diagnostic").setup({
+        preset = "modern",
+        hi = {
+          error = "DiagnosticError",
+          warn = "DiagnosticWarn",
+          info = "DiagnosticInfo",
+          hint = "DiagnosticHint",
+          arrow = "NonText",
+          background = "None",
+          mixing_color = "None",
+        },
+        signs = {
+          left = "",
+          right = "",
+          diag = "●",
+          arrow = "",
+          up_arrow = "",
+          vertical = " │",
+          vertical_end = " └",
+        },
+        blend = {
+          factor = 0.10,
+        },
+      })
+
+      -- Disable virtual text on other LSP clients
+      vim.diagnostic.config({ virtual_text = false })
+    end,
+  },
+  {
     "williamboman/mason.nvim",
     config = function()
       local mason = require("mason")
