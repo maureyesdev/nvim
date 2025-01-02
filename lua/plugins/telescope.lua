830ff4766eaf4aac91fc1bdd4ea6485972ee711b
maureyesdev <maureyes.dev@gmail.com>
2025-01-02 09:51:56 -0600

    feat: adds find with args of telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,9 +4,16 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope-ui-select.nvim",
+    { "nvim-telescope/telescope-live-grep-args.nvim", version = "^1.0.0" },
+    {
+      "isak102/telescope-git-file-history.nvim",
+      dependencies = { "tpope/vim-fugitive" },
+    },
   },
   config = function()
     local telescope = require("telescope")
+    local gfh_actions = require("telescope").extensions.git_file_history.actions
+
     telescope.setup({
       defaults = {
         vimgrep_arguments = {
@@ -49,8 +56,28 @@ return {
         ["ui-select"] = {
           require("telescope.themes").get_dropdown({}),
         },
+        -- Documentation https://github.com/BurntSushi/ripgrep/blob/master/GUIDE.md
+        -- "word" -g src/path/to/file -- -g flat for folder
+        -- "word" -t js -- -t flag for file type
+        live_grep_args = {
+          theme = "ivy",
+        },
+        git_file_history = {
+          mappings = {
+            i = {
+              ["<C-g>"] = gfh_actions.open_in_browser,
+            },
+            n = {
+              ["<C-g>"] = gfh_actions.open_in_browser,
+            },
+          },
+          browser_command = nil,
+        },
       },
     })
+
     telescope.load_extension("ui-select")
+    telescope.load_extension("git_file_history")
+    telescope.load_extension("live_grep_args")
   end,
 }
