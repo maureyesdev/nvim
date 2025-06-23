eeef2fa939e6f08f23c37b8dc7072455add0e3fc
maureyesdev <maureyes.dev@gmail.com>
2025-06-22 21:35:13 -0600

    feat: add LSP support for CSS and HTML with formatting options

---
+++ lua/plugins/lsp/html.lua
@@ -0,0 +1,47 @@
+return {
+  -----------------------------------------------------------------------------
+  -- Syntax highlight
+  -----------------------------------------------------------------------------
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "html" })
+    end,
+  },
+
+  -----------------------------------------------------------------------------
+  -- Formatting
+  -----------------------------------------------------------------------------
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft =
+        vim.tbl_deep_extend("force", opts.formatters_by_ft, {
+          html = { "prettier" },
+        })
+    end,
+  },
+
+  -----------------------------------------------------------------------------
+  -- LSP installation
+  -----------------------------------------------------------------------------
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "html-lsp", "prettier" })
+    end,
+  },
+
+  -----------------------------------------------------------------------------
+  -- LSP config
+  -----------------------------------------------------------------------------
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
+      require("lspconfig").html.setup({ capabilities = capabilities })
+    end,
+  },
+}
