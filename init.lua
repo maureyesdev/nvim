0609d090fa61b82f32cdc81276df5d82599c62ba
maureyesdev <maureyes.dev@gmail.com>
2026-04-07 13:33:42 -0600

    add treesitter

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
 require("core.opts")
 require("core.keymaps")
+
+require("plugins.treesitter")
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+vim.pack.add({
+  "https://github.com/nvim-treesitter/nvim-treesitter"
+})
+
+require("nvim-treesitter").setup({
+  ensure_installed = {},
+  auto_install = false,
+  indent = { enable = true },
+  highlight = { enable = true },
+  modules = {},
+  sync_install = true,
+  ignore_install = {},
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<C-space>",
+      node_incremental = "<C-space>",
+      scope_incremental = false,
+      node_decremental = "<bs>",
+    },
+  },
+})
