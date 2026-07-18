e80797c5c3c0da2d6fe1a63fb4969aa721d6363e
maureyesdev <maureyes.dev@gmail.com>
2026-07-17 20:50:45 -0600

    amending indent for ejs files

--- lua/packages/folding.lua
+++ lua/packages/folding.lua
@@ -1,7 +1,10 @@
 local ufo = require("ufo")
 
 ufo.setup({
-  provider_selector = function()
+  provider_selector = function(_, filetype)
+    if filetype == "ejs" then
+      return { "indent" }
+    end
     return { "lsp", "indent" }
   end,
 })
